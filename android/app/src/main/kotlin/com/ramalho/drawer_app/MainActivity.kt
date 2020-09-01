b9389e709b1599eabcb48f8c48adc9ffcf9dba5b
ramonramalho <ramonramalh@hotmail.com>
2020-09-01 19:02:01 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/ramalho/drawer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ramalho.drawer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:table_calendar/table_calendar.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  CalendarController _calendarController;
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    Map<DateTime, List<dynamic>> _events;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agenda'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Seus Compromissos ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: TableCalendar(
+                calendarController: _calendarController,
+                events: _events,
+                onDaySelected: (date,events){},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
